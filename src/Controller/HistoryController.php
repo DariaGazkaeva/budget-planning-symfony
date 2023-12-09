d9be071ba36b41e6ba4c7a27e5c2dabf27f02503
Dasha <d.gazkaeva@gmail.com>
2023-12-09 23:48:32 +0300

    Improve History page

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\MoneyOperationRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HistoryController extends AbstractController
@@ -17,10 +21,30 @@ class HistoryController extends AbstractController
         $this->userId = $this->security->getUser()->getId();
     }
     #[Route('/profile/history', name: 'history')]
-    public function index(MoneyOperationRepository $moneyOperationRepository) {
+    public function index(Request $request, MoneyOperationRepository $moneyOperationRepository) {
+        $defaults = [
+            'start' => new DateTime(date('Y-m-01')),
+            'end' => new DateTime(date('Y-m-t'))
+        ];
+        $form = $this->createFormBuilder($defaults, ['method' => 'GET'])
+            ->add('start', DateType::class, ['widget' => 'single_text'])
+            ->add('end', DateType::class, ['widget' => 'single_text'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->render("history.html.twig", [
+                'form' => $form->createView(),
+                'history_income' => $moneyOperationRepository->findByOwnerAndTypeAndPeriod($this->userId, true, $form->getData()['start'], $form->getData()['end']),
+                'history_expense' => $moneyOperationRepository->findByOwnerAndTypeAndPeriod($this->userId, false, $form->getData()['start'], $form->getData()['end'])
+            ]);
+        }
+
         return $this->render("history.html.twig", [
-            'history_income' => $moneyOperationRepository->findByOwnerAndType($this->userId, true),
-            'history_expense' => $moneyOperationRepository->findByOwnerAndType($this->userId, false)
+            'form' => $form->createView(),
+            'history_income' => $moneyOperationRepository->findByOwnerAndTypeAndPeriod($this->userId, true, $defaults['start'], $defaults['end']),
+            'history_expense' => $moneyOperationRepository->findByOwnerAndTypeAndPeriod($this->userId, false, $defaults['start'], $defaults['end'])
         ]);
     }
 }
